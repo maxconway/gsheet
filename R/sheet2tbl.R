3e47e3142ecfbfbcbcf5a4208b6212031d165dec
Max Conway <conway.max1@gmail.com>
2015-03-05 14:51:51 +0000

    Added documentation

--- R/sheet2tbl.R
+++ R/sheet2tbl.R
@@ -1,3 +1,12 @@
+#' Download Google sheet as a table
+#' 
+#' \code{sheet2tbl} wraps \code{sheet2text} to parse sheets to tables.
+#' If this function produces unexpected results, use a construction like \code{read.csv(text=google_sheet2text(url, format='csv'), stringsAsFactors=FALSE)} to parse manually, with your own options. 
+#' The Google sheet must have 'share by link' turned on.
+#' 
+#' @param url the google sheet url
+#' 
+#' @family gsheet functions
 sheet2tbl <- function(url){
-  dplyr::tbl_df(read.csv(google_sheet2text(url, format='csv'), text=., stringsAsFactors=FALSE))
+  dplyr::tbl_df(read.csv(text=google_sheet2text(url, format='csv'), stringsAsFactors=FALSE))
 }
--- R/sheet2text.R
+++ R/sheet2text.R
@@ -1,5 +1,14 @@
+#' Download Google sheet as text.
+#' 
+#' Downloads a Google sheet as text, using just the url.
+#' The Google sheet must have 'share by link' turned on.
+#' 
+#' @param url the google sheet url
+#' @param format controls the column separator used. \code{csv} or \code{tsv}
+#' 
+#' @family gsheet functions
 sheet2text <- function(url, format='csv'){
-  key <- stringr::stringr::str_replace_all(stringr::str_extract(url, '/[[:alnum:]_-]{30,}/'), '/','')
+  key <- stringr::str_replace_all(stringr::str_extract(url, '/[[:alnum:]_-]{30,}/'), '/','')
   address <- paste0('https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=',key,'&exportFormat=',format)
   page <- rvest::html('https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=1fyS4_Qoufdfy6kty5_qxuIDuvClIomn6E542vGwSF0Y&exportFormat=csv')
   content <- rvest::html_text(rvest::html_node(page, 'p'))
